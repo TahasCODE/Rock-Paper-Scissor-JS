c4b811c7fa24c9dc581b9f88189e9514c5cbc20d
TahasCODE <=>
2024-02-13 06:49:14 +0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,51 @@
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText");
+const choiceBtns = document.querySelectorAll(".choiceBtn");
+
+let player;
+let computer;
+let result;
+
+choiceBtns.forEach(button => button.addEventListener("click", () => {
+player = button.textContent;
+computerTurn();         // you can name it anything you want
+playerText.textContent = `Player: ${player}`;
+computerText.textContent = `Computer: ${computer}`;
+resultText.textContent = checkWinner();    // you can name this function anything you want tooo
+
+}));
+
+
+
+
+function computerTurn(){
+    let randNum = Math.floor(Math.random () * 3) + 1;
+    switch(randNum){
+     case 1:
+        computer = "ROCK";
+        break;
+     case 2:
+        computer = "PAPER";
+        break;
+     case 3:
+        computer = "SCISSORS";
+        break;
+    }
+}
+
+function checkWinner(){
+    if(player == computer){
+        return "DRAW!!!";
+    }
+    else if(computer == "ROCK"){
+        return (player == "PAPER") ? "YOU WON!!!!" : "YOU LOSE!!";
+    }
+    else if(computer == "PAPER"){
+        return (player == "SCISSORS") ? "YOU WON!!!!" : "YOU LOSE!!";
+    }
+    else if(computer == "SCISSORS"){
+        return (player == "ROCK") ? "YOU WON!!!!" : "YOU LOSE!!";
+    }
+}
+
